b8d38c41b75be6bbb64e88f4b2f254853e061979
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2022-08-06 11:35:29 -0400

    chore: catch all exceptions and print

--- discord-bot/discord_bot/background_schedule.py
+++ discord-bot/discord_bot/background_schedule.py
@@ -1,11 +1,19 @@
 """Helper module to run scheduled tasks in the background """
 import asyncio
+import logging
+import traceback
 
 
 async def schedule_periodically(interval_in_seconds, periodic_function, *args):
     """Helper method to schedule tasks periodically using asyncio"""
+    logger = logging.getLogger("discord_bot")
     while True:
-        await asyncio.gather(
-            asyncio.sleep(interval_in_seconds),
-            periodic_function(*args),
-        )
+        try:
+            await asyncio.gather(
+                asyncio.sleep(interval_in_seconds),
+                periodic_function(*args),
+            )
+        except Exception as exception:  # pylint: disable=broad-except
+            # Catch all exceptions for now to see what is thrown
+            logger.error("Caught an exception:\n%s\n", exception)
+            logger.error(traceback.format_exc())
